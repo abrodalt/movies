076d503ebe2f1576ae669d147430311c12fad91b
abrodalt <abrodalt@gmail.com>
2025-11-30 22:48:48 +0000

    release version 1.3

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ dbMovies = db.collection("movies")
 
 # Punto 5, inciso 1 - crear check box en el sidebar, para visualizar todos los filmes
 st.sidebar.header("Opciones")
-mostrar_todos = st.sidebar.checkbox("Mostrar todos los filmes", value=True)
+mostrar_todos = st.sidebar.checkbox("Mostrar todos los filmes", value= False)
 
 
 st.sidebar.subheader("Buscar filmes por título")
