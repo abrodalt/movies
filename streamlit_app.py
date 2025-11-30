7f504598c4b9981ef9927c1e9d1c2a0146aea92c
abrodalt <abrodalt@gmail.com>
2025-11-30 23:36:25 +0000

    release version 1.3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,24 +3,12 @@ import pandas as pd
 from google.cloud import firestore
 from google.oauth2 import service_account
 
-# Autenticación con Firebase (desde st.secrets)
+# Autentificacion 
 key_dict = st.secrets["firebase"]
 creds = service_account.Credentials.from_service_account_info(dict(key_dict))
 db = firestore.Client(credentials=creds, project=key_dict["project_id"])
 
-dbMovies = db.collection("movies")
 
-# Punto 5, inciso 1 - crear check box en el sidebar, para visualizar todos los filmes
-st.sidebar.header("Opciones")
-mostrar_todos = st.sidebar.checkbox("Mostrar todos los filmes", value= True)
-
-
-st.sidebar.subheader("Buscar filmes por título")
-titulo_buscar = st.sidebar.text_input("Título del filme :")
-btn_buscar = st.sidebar.button("Buscar filmes")
-
-
-st.header("Catalogo NETFLIX")
 
 # Cargo los datos
 @st.cache_data
@@ -34,6 +22,64 @@ def cargar_peliculas():
 
 # Se carga desde cache o si no firebase bloquea por cuotas excedidas 
 movies_dataframe = cargar_peliculas()
+
+
+
+st.sidebar.header("Opciones")
+mostrar_todos = st.sidebar.checkbox("Mostrar todos los filmes", value=True)
+
+#Buscar por titulo
+st.sidebar.subheader("Buscar filmes por titulo")
+titulo_buscar = st.sidebar.text_input("Título del filme :")
+btn_buscar = st.sidebar.button("Buscar filmes")
+
+# Para el filto de director
+st.sidebar.subheader("Seleccionar Director")
+
+# Cargar directores únicos y ordenados
+lista_directores = sorted(movies_dataframe["director"].dropna().unique())
+director_select = st.sidebar.selectbox("Director", lista_directores)
+btn_filtrar_director = st.sidebar.button("Filtrar director")
+
+# Header principal
+st.header("Catalogo NETFLIX")
+
+# Filtro por titulo
+if btn_buscar and titulo_buscar.strip() != "":
+    filtro = movies_dataframe[
+        movies_dataframe["name"].str.contains(
+            titulo_buscar.strip(), case=False, na=False
+        )
+    ]
+
+    st.header(f"Resultados de búsqueda: '{titulo_buscar}'")
+    st.dataframe(filtro)
+
+
+# Filtro por director
+elif btn_filtrar_director:
+    filtro_director = movies_dataframe[movies_dataframe["director"] == director_select]
+
+    st.header(f"Filmes del director: {director_select}")
+    st.write(f"Total filmes: {len(filtro_director)}")
+    st.dataframe(filtro_director)
+
+# Check para mostrar u ocultar
+elif mostrar_todos:
+    st.header("Todos los filmes")
+    st.dataframe(movies_dataframe)
+
+
+
+
+
+
+
+
+st.header("Catalogo NETFLIX")
+
+
+
 
 
 st.sidebar.header("Opciones")
@@ -62,11 +108,11 @@ if btn_buscar and titulo_buscar.strip() != "":
     st.dataframe(filtro)
 
 
-
-
-
-
-
 elif mostrar_todos:
     st.header("Todos los filmes")
     st.dataframe(movies_dataframe)
+
+
+
+
+
